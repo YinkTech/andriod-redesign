32f2201c2732e32296a7da4bd7a5f72712ff2d04
Olayinka Ayeni <layinka4dat@gmail.com>
2023-10-10 16:03:10 +0100

    add all files

--- src/libs/Footer.jsx
+++ src/libs/Footer.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-[#f0f3f4]' style={{padding: '40px 60px 120px 60px'}}>Footer</div>
+    <div className='bg-[#f0f3f4]' style={{padding: '40px 60px 120px 60px'}}>
+      <div>
+        <span>
+          Follow Us
+        </span>
+        <div>
+          
+        </div>
+      </div>
+      <hr />
+      <div></div>
+    </div>
   )
 }
 
