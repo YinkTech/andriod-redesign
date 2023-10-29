a4a710187eda921127c7232ac0af6dd3c4ab60aa
Olayinka Bidemi Ayeni <layinka4dat@gmail.com>
2023-10-29 19:23:02 +0100

    set footer toggle

--- src/libs/Footer.jsx
+++ src/libs/Footer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   BsFacebook,
   BsInstagram,
@@ -12,6 +12,14 @@ import andriodImg from "./../assets/img/heroFooter.png";
 import { BiPlus } from "react-icons/bi";
 
 const Footer = () => {
+  const [isBlock, setIsBlock] = useState([true, true]);
+
+  const toggleDisplay = (index) => {
+    const updatedIsBlock = [...isBlock];
+    updatedIsBlock[index] = !updatedIsBlock[index];
+    setIsBlock(updatedIsBlock);
+  };
+
   return (
     <div className="bg-[#f0f3f4] flex flex-col gap-[30px] pt-[40px] px-3 md:px-[60px] pb-0">
       <div className="container mx-auto flex flex-col gap-[40px] ">
@@ -28,130 +36,193 @@ const Footer = () => {
         <hr />
         <div>
           <div className="grid lg:grid-cols-4 gap-4 mx-auto pb-10">
-            <div className="flex flex-col gap-[24px]">
-             <div className="flex items-center justify-between">
-              <h5 className="font-[500] text-[16px] text-[#202124]">
-                {" "}
-                Privacy & Safety{" "}
-              </h5>
-              <BiPlus className="lg:hidden" />
+            <div className="flex border-b-2 pb-2 lg:border-b-0 lg:pb-0 flex-col gap-[24px]">
+              <div className="flex items-center justify-between">
+                <h5 className="font-[500] text-[16px] text-[#202124]">
+                  {" "}
+                  Privacy & Safety{" "}
+                </h5>
+                <BiPlus
+                  className="lg:hidden"
+                  onClick={() => toggleDisplay(0)}
+                />
               </div>
-              <ul className="flex flex-col gap-[20px] text-[#5F6368] text-[16px] font-[400]">
-                <li>
-                  <a href="" className="footer-nav-li">
-                    Privacy
-                  </a>
-                </li>
-                <li>Security</li>
-                <li>Physical </li>
-              </ul>
-            </div>
-            <div className="flex flex-col gap-[24px]">
-              <h5 className="font-[500] text-[16px] text-[#202124]">
-                {" "}
-                Accessibility{" "}
-              </h5>
-              <ul className="flex flex-col gap-[20px] text-[#5F6368] text-[16px] font-[400]">
-                <li>Vision features</li>
-                <li>Audio features</li>
-                <li>Mobility features</li>
-              </ul>
-            </div>
-            <div className="flex flex-col gap-[24px]">
-              <h5 className="font-[500] text-[16px] text-[#202124]">
-                {" "}
-                More from Android{" "}
-              </h5>
-              <div className="flex flex-col gap-[20px] text-[#5F6368] text-[16px] font-[400]">
-                <span>Android TV</span>
-                <span>Transfer contacts</span>
-                <span>Get The Message </span>
+              <div className={`content ${isBlock[0] ? " " : "slide-in"}`}>
+                <ul className="flex flex-col gap-[20px] text-[#5F6368] text-[16px] font-[400]">
+                  <li>
+                    <a href="" className="footer-nav-li">
+                      Privacy
+                    </a>
+                  </li>
+                  <li>Security</li>
+                  <li>Physical </li>
+                </ul>
               </div>
             </div>
-            <div className="flex flex-col gap-[24px]">
-              <h5 className="font-[500] text-[16px] text-[#202124]">
-                {" "}
-                Support{" "}
-              </h5>
-              <div className="flex flex-col gap-[20px] text-[#5F6368] text-[16px] font-[400]">
-                <li className="flex items-center gap-[8px]">
+            <div className="flex border-b-2 pb-2 lg:border-b-0 lg:pb-0 flex-col gap-[24px]">
+              <div className="flex items-center justify-between">
+                <h5 className="font-[500] text-[16px] text-[#202124]">
                   {" "}
-                  Help Center <TbArrowUpRight />
-                </li>
-                <li className="flex items-center gap-[8px]"> File Transfer </li>
-                <li className="flex items-center gap-[8px]">
-                  {" "}
-                  Find my device <TbArrowUpRight />
-                </li>
-                <li className="flex items-center gap-[8px]">
-                  {" "}
-                  Join user studies <TbArrowUpRight />
-                </li>
+                  Accessibility{" "}
+                </h5>
+                <BiPlus
+                  className="lg:hidden"
+                  onClick={() => toggleDisplay(1)}
+                />
+              </div>
+              <div className={`content ${isBlock[1] ? " " : "slide-in"}`}>
+                <ul className="flex flex-col gap-[20px] text-[#5F6368] text-[16px] font-[400]">
+                  <li>Vision features</li>
+                  <li>Audio features</li>
+                  <li>Mobility features</li>
+                </ul>
               </div>
             </div>
-            <div className="flex flex-col gap-[24px]">
-              <h5 className="font-[500] text-[16px] text-[#202124]">
-                {" "}
-                For Enterprise{" "}
-              </h5>
-              <ul className="flex flex-col gap-[20px] text-[#5F6368] text-[16px] font-[400]">
-                <li>overview</li>
-                <li>Find solutions</li>
-                <li> Enterprise Devices </li>
-                <li> Enterprise Support </li>
-                <li className="flex items-center gap-[8px]">
+            <div className="flex border-b-2 pb-2 lg:border-b-0 lg:pb-0 flex-col gap-[24px]">
+              <div className="flex items-center justify-between">
+                <h5 className="font-[500] text-[16px] text-[#202124]">
                   {" "}
-                  Enterprise Blog <TbArrowUpRight />
-                </li>
-              </ul>
+                  More from Android{" "}
+                </h5>
+
+                <BiPlus
+                  className="lg:hidden"
+                  onClick={() => toggleDisplay(2)}
+                />
+              </div>
+
+              <div className={`content ${isBlock[2] ? "slide-in" : ""}`}>
+                <ul className="flex flex-col gap-[20px] text-[#5F6368] text-[16px] font-[400]">
+                  <li>Android TV</li>
+                  <li>Transfer contacts</li>
+                  <li>Get The Message </li>
+                </ul>
+              </div>
             </div>
-            <div className="flex flex-col gap-[24px]">
-              <h5 className="font-[500] text-[16px] text-[#202124]">
-                {" "}
-                For the press{" "}
-              </h5>
-              <ul className="flex flex-col gap-[20px] text-[#5F6368] text-[16px] font-[400]">
-                <li className="flex items-center gap-[8px]">
+            <div className="flex border-b-2 pb-2 lg:border-b-0 lg:pb-0 flex-col gap-[24px]">
+              <div className="flex items-center justify-between">
+                <h5 className="font-[500] text-[16px] text-[#202124]">
                   {" "}
-                  Android blog <TbArrowUpRight />
-                </li>
-                <li className="flex items-center gap-[8px]">
-                  {" "}
-                  Press Corner <TbArrowUpRight />
-                </li>
-                <li className="flex items-center gap-[8px]">
-                  {" "}
-                  Contact Press team <TbArrowUpRight />
-                </li>
-              </ul>
+                  Support{" "}
+                </h5>
+
+                <BiPlus
+                  className="lg:hidden"
+                  onClick={() => toggleDisplay(3)}
+                />
+              </div>
+
+              <div className={`content ${isBlock[3] ? " slide-in" : ""}`}>
+                <ul className="flex flex-col gap-[20px] text-[#5F6368] text-[16px] font-[400]">
+                  <li className="flex items-center gap-[8px]">
+                    {" "}
+                    Help Center <TbArrowUpRight />
+                  </li>
+                  <li className="flex items-center gap-[8px]">
+                    {" "}
+                    File Transfer{" "}
+                  </li>
+                  <li className="flex items-center gap-[8px]">
+                    {" "}
+                    Find my device <TbArrowUpRight />
+                  </li>
+                  <li className="flex items-center gap-[8px]">
+                    {" "}
+                    Join user studies <TbArrowUpRight />
+                  </li>
+                </ul>
+              </div>
             </div>
-            <div className="flex flex-col gap-[24px]">
-              <h5 className="font-[500] text-[16px] text-[#202124]">
-                {" "}
-                For developers{" "}
-              </h5>
-              <ul className="flex flex-col gap-[20px] text-[#5F6368] text-[16px] font-[400]">
-                <li className="flex items-center gap-[8px]">
+            <div className="flex border-b-2 pb-2 lg:border-b-0 lg:pb-0 flex-col gap-[24px]">
+              <div className="flex items-center justify-between">
+                <h5 className="font-[500] text-[16px] text-[#202124]">
                   {" "}
-                  Developer Resources <TbArrowUpRight />
-                </li>
-                <li className="flex items-center gap-[8px]">
+                  For Enterprise{" "}
+                </h5>
+                <BiPlus
+                  className="lg:hidden"
+                  onClick={() => toggleDisplay(4)}
+                />
+              </div>
+
+              <div className={`content ${isBlock[4] ? "slide-in" : ""}`}>
+                <ul className="flex flex-col gap-[20px] text-[#5F6368] text-[16px] font-[400]">
+                  <li>overview</li>
+                  <li>Find solutions</li>
+                  <li> Enterprise Devices </li>
+                  <li> Enterprise Support </li>
+                  <li className="flex items-center gap-[8px]">
+                    {" "}
+                    Enterprise Blog <TbArrowUpRight />
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="flex border-b-2 pb-2 lg:border-b-0 lg:pb-0 flex-col gap-[24px]">
+              <div className="flex items-center justify-between">
+                <h5 className="font-[500] text-[16px] text-[#202124]">
                   {" "}
-                  Android Studio and SDK <TbArrowUpRight />
-                </li>
-                <li className="flex items-center gap-[8px]">
+                  For the press{" "}
+                </h5>
+                <BiPlus
+                  className="lg:hidden"
+                  onClick={() => toggleDisplay(5)}
+                />
+              </div>
+
+              <div className={`content ${isBlock[5] ? "slide-in" : ""}`}>
+                <ul className="flex flex-col gap-[20px] text-[#5F6368] text-[16px] font-[400]">
+                  <li className="flex items-center gap-[8px]">
+                    {" "}
+                    Android blog <TbArrowUpRight />
+                  </li>
+                  <li className="flex items-center gap-[8px]">
+                    {" "}
+                    Press Corner <TbArrowUpRight />
+                  </li>
+                  <li className="flex items-center gap-[8px]">
+                    {" "}
+                    Contact Press team <TbArrowUpRight />
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="flex border-b-2 pb-2 lg:border-b-0 lg:pb-0 flex-col gap-[24px]">
+              <div className="flex items-center justify-between">
+                <h5 className="font-[500] text-[16px] text-[#202124]">
                   {" "}
-                  Android Open Source Project <TbArrowUpRight />
-                </li>
-                <li className="flex items-center gap-[8px]">
-                  {" "}
-                  How Google Play works <TbArrowUpRight />
-                </li>
-              </ul>
+                  For developers{" "}
+                </h5>
+                <BiPlus
+                  className="lg:hidden"
+                  onClick={() => toggleDisplay(6)}
+                />
+              </div>
+
+              <div className={`content ${isBlock[6] ? "slide-in" : " "}`}>
+                <ul className="flex flex-col gap-[20px] text-[#5F6368] text-[16px] font-[400]">
+                  <li className="flex items-center gap-[8px]">
+                    {" "}
+                    Developer Resources <TbArrowUpRight />
+                  </li>
+                  <li className="flex items-center gap-[8px]">
+                    {" "}
+                    Android Studio and SDK <TbArrowUpRight />
+                  </li>
+                  <li className="flex items-center gap-[8px]">
+                    {" "}
+                    Android Open Source Project <TbArrowUpRight />
+                  </li>
+                  <li className="flex items-center gap-[8px]">
+                    {" "}
+                    How Google Play works <TbArrowUpRight />
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
 
-          <hr />
+          <hr className="hidden lg:flex" />
 
           <div>
             <div className="my-9 flex flex-col lg:flex-row gap-[20px] lg:gap-[120px] justify-between items-start">
